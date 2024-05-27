31273407c129fb5c4f74d99cef6d6175ebb1357e
hunhee99 <hunhee9982@gmail.com>
2024-05-27 10:54:22 +0900

    1065

--- 1065.py
+++ 1065.py
@@ -1,15 +1,16 @@
 n = int(input())
 cnt = 0
 if n == 1000:
-    n = 999
-for i in range(1, n + 1):
-    if i < 100:
-        cnt += 1
-    elif i < 1000:
-        a = i % 10
-        b = (i % 100) - a
-        c = i // 100
-        result = (c - b) - (b - a)
-        if result == 0:
+    cnt = 144
+else:    
+    for i in range(1, n + 1):
+        if i < 100:
             cnt += 1
+        elif i < 1000:
+            a = i % 10
+            b = ((i % 100) - a) // 10
+            c = i // 100
+            result = (c - b) - (b - a)
+            if result == 0:
+                cnt += 1
 print(cnt)      
